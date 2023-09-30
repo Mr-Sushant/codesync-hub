c565ae65ecd296436dd54b1c845443fa05e58adc
Sushant Shukla <1805352@kiit.ac.in>
2023-09-30 21:19:22 +0530

    UI not fixed

---
+++ src/constants/judge.js
@@ -0,0 +1,3 @@
+export const key = 'd9a5c9191amshdf151fe5ccd39e0p1ef7b0jsn747189c0bcfb';
+export const host = 'judge0-ce.p.rapidapi.com';
+export const submissionURL = 'https://judge0-ce.p.rapidapi.com/submissions';
